b5543012eef1b24fd999e262613b1ba6e3300183
LindseySongstad <lindsey.songstad@outlook.com>
2020-10-14 12:13:03 -0700

    functions

---
+++ Basic JavaScript/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+
+var x= 'x', y='y', z='z';
+
+var x = x.fontcolor('red'), y=y.fontcolor('yellow')
+
+
+document.write(x + y +z);
+
+
---
+++ Basic JavaScript/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+
+
+function myFunction() { // function to change p text
+    var response = 'You pushed '
+    response += 'a button!'
+    document.getElementById('push').innerHTML = response ;
+};
