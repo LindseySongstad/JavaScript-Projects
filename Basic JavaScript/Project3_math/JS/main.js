eca150d0c8a2c8d5cc05fd54b376381f9e1755a9
LindseySongstad <lindsey.songstad@outlook.com>
2020-12-18 10:11:48 -0800

    JS 86
    JS 86

--- Basic JavaScript/Project3_math/JS/main.js
+++ Basic JavaScript/Project3_math/JS/main.js
@@ -1,33 +1,50 @@
 
+var x = 8;  // declare variables to perfrom math opperations on
+var y = 6;
 
 
-function myFunction(x,y) { 
+function addition(x,y) { // addition function
     result = x + y;
-    document.getElementById('math').innerHTML = result;
+    document.getElementById('math').innerHTML = "Addition " +result;
 };
 
-myFunction(3,4);
+addition(x,y);
 
 
-function subtraction() {
-    result = 5 - 2;
-    document.getElementById('math2').innerHTML = result;
+function subtraction(x,y) {  // subtraction function
+    result =  x - y;
+    document.getElementById('math1').innerHTML =  "Subtraction " + result;
 };
 
-subtraction();
+subtraction(x,y);
 
-function modulus(){
-    var mod = 25 % 6;
-    document.getElementById('math3').innerHTML = "remainder of 25 / 6 is " + mod;
+function multiplication(x,y) {  // subtraction function
+    result =  x * y;
+    document.getElementById('math2').innerHTML = "Multiplication " + result;
+};
+
+multiplication(x,y);
+
+function modulus(x,y){
+    var mod = x % y;
+    document.getElementById('math3').innerHTML = "Modulus " + mod;
 }
 
-modulus();
+modulus(x,y);
 
-var num = (Math.random());
 
-document.writeln("num: " + num);
-num++;
+var num = (Math.random());  // get random number
 
-document.writeln("num plus 1: " +num);
+document.getElementById('math4').innerHTML = "Random number: " + num;
 
-document.writeln(Math.sqrt(64));
+num++;  // add 1 to random number
+document.getElementById('math5').innerHTML = "Random number plus 1: " +num;
+
+num--;
+num--; // subtract 1 from random number
+document.getElementById('math6').innerHTML = "Random number minus 1: " +num;
+
+
+document.getElementById('math7').innerHTML = "The square root of 64 is " + Math.sqrt(64);
+
+
