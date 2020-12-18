7eb7c07dda42b50c1d3e1b6218e6d382881c4edd
LindseySongstad <lindsey.songstad@outlook.com>
2020-12-18 08:58:28 -0800

    JS 59
    JS 59

--- Basic JavaScript/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,12 @@
 
-var x= 'x', y='y', z='z';
+var x= 'Hello ', y='there ', z='world!';  // declare variables
 
-var x = x.fontcolor('red'), y=y.fontcolor('yellow')
-
-
-document.write(x + y +z);
+var x = x.fontcolor('red'), y=y.fontcolor('yellow')  // assign color to variables
 
 
+document.write(x + y +z); // print variables
+
+function myFunction() {  // function runs when button is pushed
+    document.getElementById('push').innerHTML = 'You pushed a button!';
+    window.alert("You pushed a button!");
+}
