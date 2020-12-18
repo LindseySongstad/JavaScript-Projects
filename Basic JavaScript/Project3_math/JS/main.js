62b35d4b590cac82e3293881027cbb3758cefaa1
LindseySongstad <lindsey.songstad@outlook.com>
2020-12-18 12:37:45 -0800

    J86 with button
    J86 with button

--- Basic JavaScript/Project3_math/JS/main.js
+++ Basic JavaScript/Project3_math/JS/main.js
@@ -1,6 +1,5 @@
 
-var x = 8;  // declare variables to perfrom math opperations on
-var y = 6;
+
 
 
 function addition(x,y) { // addition function
@@ -8,7 +7,7 @@ function addition(x,y) { // addition function
     document.getElementById('math').innerHTML = "Addition " +result;
 };
 
-addition(x,y);
+
 
 
 function subtraction(x,y) {  // subtraction function
@@ -16,35 +15,45 @@ function subtraction(x,y) {  // subtraction function
     document.getElementById('math1').innerHTML =  "Subtraction " + result;
 };
 
-subtraction(x,y);
+
 
 function multiplication(x,y) {  // subtraction function
     result =  x * y;
     document.getElementById('math2').innerHTML = "Multiplication " + result;
 };
 
-multiplication(x,y);
+
 
 function modulus(x,y){
     var mod = x % y;
     document.getElementById('math3').innerHTML = "Modulus " + mod;
 }
 
-modulus(x,y);
 
 
-var num = (Math.random());  // get random number
+function doMath(){  // executes all math functions when button is pushed
+    var x = 8;  // declare variables to perfrom math opperations on
+    var y = 6;
 
-document.getElementById('math4').innerHTML = "Random number: " + num;
+    addition(x,y);
+    subtraction(x,y);
+    multiplication(x,y);
+    modulus(x,y);
 
-num++;  // add 1 to random number
-document.getElementById('math5').innerHTML = "Random number plus 1: " +num;
+    var num = (Math.random());  // get random number
 
-num--;
-num--; // subtract 1 from random number
-document.getElementById('math6').innerHTML = "Random number minus 1: " +num;
+    document.getElementById('math4').innerHTML = "Random number: " + num;
+
+    num++;  // add 1 to random number
+    document.getElementById('math5').innerHTML = "Random number plus 1: " +num;
+
+    num--;
+    num--; // subtract 1 from random number
+    document.getElementById('math6').innerHTML = "Random number minus 1: " +num;
 
 
-document.getElementById('math7').innerHTML = "The square root of 64 is " + Math.sqrt(64);
+    document.getElementById('math7').innerHTML = "The square root of 64 is " + Math.sqrt(64);
+}
+
 
 
